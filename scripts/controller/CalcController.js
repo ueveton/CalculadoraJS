90a60d65f3716ea25f203d4c5a6ee19cefff6868
ueveton <uevetonsp89@gmail.com>
2023-01-04 12:19:13 -0300

    inicializando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,14 @@ class CalcController {
 
     constructor(){
         this._displayCalc = '0';
+        this.initialize();
+    }
+
+    initialize (){
+        let displayCalEl = document.querySelector('#resultado');
+
+        displayCalEl.innerHTML = '1253';
+
     }
 
     get displayCal (){

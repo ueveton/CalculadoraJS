11a24ac039ff91bbc425a3027b90ce3a2bc7d0c4
ueveton <uevetonsp89@gmail.com>
2023-01-21 11:12:10 -0300

    add switch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,8 @@
 class CalcController {
 
     constructor(){
-        this._displayCalEl = document.querySelector('#resultado');
+        this._operation = [];
+        this._displayCalcEl = document.querySelector('#resultado');
         //this._displayCalc = '0';
         this.initialize();
     }
@@ -12,12 +13,66 @@ class CalcController {
         });
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    addOperation (value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    errorCalc(){
+        this.displayCal = 'Error';
+    }
+
+    execBtn(value){
+        switch (value) {
+            case 'c':
+                this.clearAll();
+            break;
+            case 'soma':
+                
+            break;
+            case 'subtracao':
+                
+            break;
+            case 'multiplicacao':
+                
+            break;
+            case 'divisao':
+                
+            break;
+            case 'igual':
+                
+            break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.errorCalc();
+            break;
+        }
+    }
+
     initialize (){
         let buttons = document.querySelectorAll('td > button');
 
         buttons.forEach(btn=>{
             this.addEventListenerAll(btn, 'click drag', e=>{
-                console.log(btn.className.replace('btn-',''));
+                
+                let textBtn = btn.className.replace('btn-','');
+                this.execBtn(textBtn);
+                
             });
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
                 btn.style.cursor = 'pointer';

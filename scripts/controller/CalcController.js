30a30a721ad9440a00c0e3e847938f1335d0763d
ueveton <uevetonsp89@gmail.com>
2023-02-01 12:16:28 -0300

    isNaN com Array

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -17,8 +17,35 @@ class CalcController {
         this._operation = [];
     }
 
+    getLastOperation(){
+        return this._operation[this._operation.length-1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length -1] = value;
+    }
+
+    isOperator(value){
+        return(['+','-','/','*'].indexOf(value) > -1);
+    }
+
     addOperation (value){
-        this._operation.push(value);
+
+        if (isNaN(this.getLastOperation())){
+
+            if(this.isOperator(value)){
+                this._setLastOperation(value);
+            } else if(isNaN(value)) {
+                console.log(value);
+            } else{
+                this._operation.push(value);
+            }
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+
+        }
         console.log(this._operation);
     }
 
@@ -32,20 +59,24 @@ class CalcController {
                 this.clearAll();
             break;
             case 'soma':
-                
+                this.addOperation('+');
             break;
             case 'subtracao':
-                
+                this.addOperation('-');
             break;
             case 'multiplicacao':
-                
+                this.addOperation('*');
             break;
             case 'divisao':
-                
+                this.addOperation('/');
             break;
             case 'igual':
                 
             break;
+            case 'ponto':
+                this.addOperation('.');
+            break;
+
             case '0':
             case '1':
             case '2':
